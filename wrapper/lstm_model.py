f13b5d4fd6cca6ea2c7c6e1f52732ebef739ced7
luvitchumber <luvitchumber@gmail.com>
2020-04-01 13:04:41 -0400

    added verbose flag to init function

--- wrapper/lstm_model.py
+++ wrapper/lstm_model.py
@@ -36,7 +36,7 @@ class LSTMModel:
     def load_network(self, filepath):
         self.model = load_model(filepath, compile=True)
 
-    def init_network(self, hidden_size, activation='sigmoid', optimizer='adam', loss='mean_squared_error'):
+    def init_network(self, hidden_size, activation='sigmoid', optimizer='adam', loss='mean_squared_error', verbose=False):
         self.model = Sequential()
 
         self.model.add(LSTM(units=hidden_size, return_sequences=True, input_shape=(self.input_length, 1)))
@@ -50,8 +50,8 @@ class LSTMModel:
         self.model.add(Dense(units=self.steps, activation=activation))
 
         self.model.compile(optimizer=optimizer, loss=loss)
-
-        print(self.model.summary())
+        if verbose:
+            print(self.model.summary())
 
     def train(self, x_train, y_train, file_loc, validation_split=0.2, save=True):
         check_pointer = ModelCheckpoint(filepath=file_loc, verbose=1)

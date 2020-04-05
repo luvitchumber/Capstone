2736123cca44cd45bc2d7a66bdb288e3174bafba
sesinclair <15ss46@queensu.ca>
2020-04-05 00:21:31 -0400

    small change

--- wrapper/main.py
+++ wrapper/main.py
@@ -223,7 +223,5 @@ def main():
 			x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 			res = model.predict(x_test, num_inter)
 			print("Prediction: " + str(res))
-			#sleep(10)
-			
 			
 main()

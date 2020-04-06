629edb2e8db15538e751ba9c82daf5130f9dc5b7
sesinclair <33702897+sesinclair@users.noreply.github.com>
2020-04-05 22:00:52 -0400

    added some changes for multi model mode and implemented option 5

--- wrapper/lstm_model.py
+++ wrapper/lstm_model.py
@@ -1,84 +1,128 @@
+'''
+import tensorflow 
+from tensorflow import keras as keras
+from tensorflow.keras.models import Sequential, load_model, save_model
+from tensorflow.keras.layers import TimeDistributed, Dense, LSTM, Activation, RepeatVector, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.utils import  to_categorical
+'''
+
+#'''
 from keras.models import Sequential, load_model, save_model
 from keras.layers import TimeDistributed, Dense, LSTM, Activation, RepeatVector, Dropout
 from keras.callbacks import ModelCheckpoint
 from keras.utils import  to_categorical
+#'''
 import numpy as np
 
 
 # steps depends on amount of data to be predicted
 class LSTMModel:
-    def __init__(self, x_train_shape, y_train_shape, num_epochs=50, l_rate=.001, batch_size=50):
-        self.model = None
-        self.current_idx = 0
-        self.input_length = x_train_shape
-        self.steps = y_train_shape
-        self.epochs = num_epochs
-        self.l_rate = l_rate
-        self.batch_size = batch_size
+	def __init__(self, x_train_shape, y_train_shape, intersection_list=None, num_epochs=50, l_rate=.001, batch_size=50):
+		self.model = None
+		self.models = []
+		self.multi_model = False
+		self.current_idx = 0
+		self.input_length = x_train_shape
+		self.steps = y_train_shape
+		self.epochs = num_epochs
+		self.l_rate = l_rate
+		self.batch_size = batch_size
+		if intersection_list:
+			self.intersections=intersection_list
+			self.multi_model = True
+			self.load_network()
+			
 
-    def get_batch(self, x_data, y_data):
-        x = np.zeros(self.batch_size)
-        y = np.zeros(self.batch_size)
-        while True:
-            if self.current_idx >= len(self.x_data):
-                # reset the index back to the start of the data set
-                self.current_idx = 0
-            x = x_data[self.current_idx:self.current_idx + self.batch_size]
-            y_tmp = y_data[self.current_idx:self.current_idx + self.batch_size]
-            y = to_categorical(y_tmp, num_classes=self.possible_classes)
-            # convert all of temp_y into a one hot representation
-            self.current_idx += self.batch_size
-            yield x, y
+	def get_batch(self, x_data, y_data):
+		x = np.zeros(self.batch_size)
+		y = np.zeros(self.batch_size)
+		while True:
+			if self.current_idx >= len(self.x_data):
+				# reset the index back to the start of the data set
+				self.current_idx = 0
+			x = x_data[self.current_idx:self.current_idx + self.batch_size]
+			y_tmp = y_data[self.current_idx:self.current_idx + self.batch_size]
+			y = to_categorical(y_tmp, num_classes=self.possible_classes)
+			# convert all of temp_y into a one hot representation
+			self.current_idx += self.batch_size
+			yield x, y
 
-    def save_network(self, filepath):
-        save_model(self.model, filepath, overwrite=True, include_optimizer=True)
+	def save_network(self, filepath):
+		save_model(self.model, filepath, overwrite=True, include_optimizer=True)
 
-    def load_network(self, filepath):
-        self.model = load_model(filepath, compile=True)
+	def load_network(self, filepath=''):
+		if self.multi_model:
+			for i in self.intersections:
+				self.models.append(load_model("./model/" + i + ".hdf"))
+		else:
+			self.model = load_model(filepath, compile=True)
+			
+	def init_network(self, hidden_size, activation='relu', optimizer='adam', loss='mean_squared_error', verbose=False):
+		model = Sequential()
+		model.add(LSTM(units=hidden_size, return_sequences=True, input_shape=(self.input_length, 1)))
+		model.add(LSTM(units=hidden_size, return_sequences=True))
+		model.add(LSTM(units=hidden_size, return_sequences=True))
+		model.add(LSTM(units=hidden_size))
+		model.add(Dense(units=self.steps, activation=activation))
+		model.compile(optimizer=optimizer, loss=loss)
+		
+		if verbose:
+			print(model.summary())
+		
+		if self.multi_model:
+			self.models.append(model)
+		else:
+			self.model = model
 
-    def init_network(self, hidden_size, activation='linear', optimizer='adam', loss='mean_squared_error', verbose=False):
-        self.model = Sequential()
 
-        self.model.add(LSTM(units=hidden_size, return_sequences=True, input_shape=(self.input_length, 1)))
+	def train(self, x_train, y_train, file_loc, validation_split=0.2, save=True):
+		check_pointer = ModelCheckpoint(filepath=file_loc, verbose=1)
+		callbacks_list = [check_pointer]
+		if self.multi_model:
+			print("Training is not possible with multi model mode!")
+		else:
+			self.model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size,
+					   validation_split=validation_split, callbacks=callbacks_list)
+		if save:
+			print("Saving Trained Model")
+			self.save_network(file_loc)
 
-        self.model.add(LSTM(units=hidden_size, return_sequences=True))
+	def get_accuracy(self, x_data, y_data, verbose=False):
+		if self.multi_model:
+			try:
+				test_output = self.model[self.intersections.index(x_data[0][0])].predict(x_data)
+			except:
+				print("Model for intersection does not exist!")
+		else:
+			test_output = self.model.predict(x_data)
+		test_output = np.around(test_output, 0).astype(int)
+		corr = 0
+		incorr = 0
+		for pred, label in zip(test_output, y_data):
+			pred = pred.astype(int)
+			label = label.astype(int)
+			for i, j in zip(pred, label):
+				if i == j:
+					corr = corr + 1
+				else:
+					incorr = incorr + 1
+				if verbose == 1:
+					print("Label:", label)
+					print("Prediction:", pred)
 
-        self.model.add(LSTM(units=hidden_size, return_sequences=True))
-
-        self.model.add(LSTM(units=hidden_size))
-
-        self.model.add(Dense(units=self.steps, activation=activation))
-
-        self.model.compile(optimizer=optimizer, loss=loss)
-        if verbose:
-            print(self.model.summary())
-
-    def train(self, x_train, y_train, file_loc, validation_split=0.2, save=True):
-        check_pointer = ModelCheckpoint(filepath=file_loc, verbose=1)
-        callbacks_list = [check_pointer]
-        self.model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size,
-                       validation_split=validation_split, callbacks=callbacks_list)
-        if save:
-            print("Saving Trained Model")
-            self.save_network(file_loc)
-
-    def predict(self, x_data, y_data, verbose=False):
-        test_output = self.model.predict(x_data)
-        test_output = np.around(test_output, 0).astype(int)
-        corr = 0
-        incorr = 0
-        for pred, label in zip(test_output, y_data):
-            pred = pred.astype(int)
-            label = label.astype(int)
-            for i, j in zip(pred, label):
-                if i == j:
-                    corr = corr + 1
-                else:
-                    incorr = incorr + 1
-                if verbose == 1:
-                    print("Label:", label)
-                    print("Prediction:", pred)
-
-        res = corr / (corr + incorr) * 100
-        print(f"Accuracy rate: {res}")
-        return test_output
+		res = corr / (corr + incorr) * 100
+		print("Accuracy rate: {res}")
+		return test_output
+	
+	def predict(self, x_data, intersection=0):
+		test_output = 0
+		if self.multi_model:
+			try:
+				test_output = self.models[self.intersections.index(str(intersection))].predict(x_data)
+			except:
+				print("Model for intersection does not exist!")
+		else:
+			test_output = self.model.predict(x_data)
+		test_output = np.around(test_output, 0).astype(int)
+		return test_output
--- wrapper/main.py
+++ wrapper/main.py
@@ -2,144 +2,230 @@ from data_processing import *
 from lstm_model import LSTMModel
 from dict_bin_search import search
 import pandas as pd
+import numpy as np
 import os.path
 import menu
-
+import re
+import sys
 
 def get_dataset(sel_intersection, intersections):
-    intersection_file = search(intersections, 'name', sel_intersection)
-    intersection_file = intersections[intersection_file]['dataset']
-    df = pd.read_csv(intersection_file)
-    return df
+	intersection_file = search(intersections, 'name', sel_intersection)
+	intersection_file = intersections[intersection_file]['dataset']
+	df = pd.read_csv(intersection_file.strip(' '))
+	return df
 
 
 def main():
-    """Program execution starts here."""
-    intersections = read_intersections("intersections.data")
-    #sel_intersection = intersections[0]['name']
-    sel_intersection = "4589"
+	"""Program execution starts here."""
+	intersections = read_intersections("intersections.data")
+	intersection_list = []
+	
+	for i in range(len(intersections)):
+		intersection_list.append(intersections[i]['name'].strip(' '))
+		
+	#sel_intersection = intersections[0]['name']
+	sel_intersection = "4589"
 
-    df = get_dataset(sel_intersection, intersections)
-    x_train, x_test, y_train, y_test = preprocessing(df)
+	df = get_dataset(sel_intersection, intersections)
+	x_train, x_test, y_train, y_test = preprocessing(df)
 
-    model = LSTMModel(x_train.shape[1], y_train.shape[1])
-    model_file = search(intersections, 'name', sel_intersection)
+	model_file = search(intersections, 'name', sel_intersection)
 
-    main_menu = [
-        "CAPSTONE TRAFFIC PREDICTION",
+	main_menu = [
+		"CAPSTONE TRAFFIC PREDICTION",
 
-        "Select Intersection",
-        "List of Intersections",
-        "Train Intersection",
-        "Test Intersection (Accuracy)",
-        "Route Check (Not Implemented Yet)"
-    ]
-    train_menu = [
-        "Train Mode:",
+		"Select Intersection",
+		"List of Intersections",
+		"Train Intersection",
+		"Test Intersection (Accuracy)",
+		"Route Check"
+	]
+	train_menu = [
+		"Train Mode:",
 
-        "Train from scratch w/ events",
-        "Train from file w/ events",
-        "Train from scratch",
-        "Train from file",
-    ]
+		"Train from scratch w/ events",
+		"Train from file w/ events",
+		"Train from scratch",
+		"Train from file",
+	]
+	route_check_menu = [
+		"Train Mode:",
 
-    while True:
-        print("Currently Selected Intersection:", sel_intersection)
-        choice = menu.do_menu(main_menu)
-        if choice is None:
-            return  # Exit main() (and program).
-        if choice == 1:
-            # Select Intersection
-            temp_menu = ["Please Select a New Intersection"]
+		"Train from scratch w/ events",
+		"Train from file w/ events",
+		"Train from scratch",
+		"Train from file",
+	]
 
-            for line in intersections:
-                option = line["name"] + ": " + line["street"]
-                temp_menu.append(option)
+	while True:
+		print("Currently Selected Intersection:", sel_intersection)
+		choice = menu.do_menu(main_menu)
+		model = LSTMModel(x_train.shape[1], y_train.shape[1])
+		if choice is None:
+			return  # Exit main() (and program).
+		if choice == 1:
+			# Select Intersection
+			temp_menu = ["Please Select a New Intersection"]
 
-            choice = menu.do_menu(temp_menu)
-            if choice is not None:
-                sel_intersection = intersections[choice - 1]['name']
-                print(sel_intersection, "set as current intersection.")
+			for line in intersections:
+				option = line["name"] + ": " + line["street"]
+				temp_menu.append(option)
 
-                df = get_dataset(sel_intersection, intersections)
-                x_train, x_test, y_train, y_test = preprocessing(df)
+			choice = menu.do_menu(temp_menu)
+			if choice is not None:
+				sel_intersection = intersections[choice - 1]['name']
+				print(sel_intersection, "set as current intersection.")
 
-                model = LSTMModel(x_train.shape[1], y_train.shape[1])
-                model_file = search(intersections, 'name', sel_intersection)
-                if model_file is not None:
-                    model_file = intersections[model_file]['model']
-                    model.load_network(model_file)
+				df = get_dataset(sel_intersection, intersections)
+				x_train, x_test, y_train, y_test = preprocessing(df)
 
-        elif choice == 2:
-            # List Intersections
-            print_intersections(intersections)
-        elif choice == 3:
-            # Train Intersections
-            # TODO test each option
-            choice = menu.do_menu(train_menu)
-            if choice == 1:
-                x_train, x_test, y_train, y_test = preprocessing(df, events=True)
-                model = LSTMModel(x_train.shape[1], y_train.shape[1])
+				model_file = search(intersections, 'name', sel_intersection)
+				if model_file is not None:
+					model_file = intersections[model_file]['model']
+					#try:
+					model.load_network('./model/'+str(sel_intersection)+'.hdf')
+					#except:
+					#	print("File for loading model is not found! Creating a new model from scratch")
+					#	model.init_network(hidden_size=50)
 
-                intersection_idx = search(intersections, 'name', sel_intersection)
-                model_file = "model/" + sel_intersection + ".hdf"
-                if os.path.exists(model_file):
-                    os.remove(model_file)
-                model.init_network(hidden_size=50)
-            elif choice == 2:
-                x_train, x_test, y_train, y_test = preprocessing(df, events=True)
-                model = LSTMModel(x_train.shape[1], y_train.shape[1])
+		elif choice == 2:
+			# List Intersections
+			print_intersections(intersections)
+		elif choice == 3:
+			model = LSTMModel(x_train.shape[1], y_train.shape[1])
+			# Train Intersections
+			# TODO test each option
+			choice = menu.do_menu(train_menu)
+			if choice == 1:
+				x_train, x_test, y_train, y_test = preprocessing(df, events=True)
+				model = LSTMModel(x_train.shape[1], y_train.shape[1])
 
-                intersection_idx = search(intersections, 'name', sel_intersection)
-                model_file = "model/" + sel_intersection + ".hdf"
-                if os.path.exists(model_file):
-                    model.load_network(model_file)
-                else:
-                    print("Model does not exist, starting from scratch")
-                    model.init_network(hidden_size=50)
-            elif choice == 3:
-                x_train, x_test, y_train, y_test = preprocessing(df, events=False)
-                model = LSTMModel(x_train.shape[1], y_train.shape[1])
+				intersection_idx = search(intersections, 'name', sel_intersection)
+				model_file = "model/" + sel_intersection + ".hdf"
+				#if os.path.exists(model_file):
+					#os.remove(model_file)
+				model.init_network(hidden_size=50)
+			elif choice == 2:
+				x_train, x_test, y_train, y_test = preprocessing(df, events=True)
+				model = LSTMModel(x_train.shape[1], y_train.shape[1])
 
-                intersection_idx = search(intersections, 'name', sel_intersection)
-                model_file = "model/" + sel_intersection + ".hdf"
-                if os.path.exists(model_file):
-                    os.remove(model_file)
-                model.init_network(hidden_size=50)
-            elif choice == 4:
-                x_train, x_test, y_train, y_test = preprocessing(df, events=False)
-                model = LSTMModel(x_train.shape[1], y_train.shape[1])
+				intersection_idx = search(intersections, 'name', sel_intersection)
+				model_file = "model/" + sel_intersection + ".hdf"
+				if os.path.exists(model_file):
+					model.load_network(model_file)
+				else:
+					print("Model does not exist, starting from scratch")
+					model.init_network(hidden_size=50)
+			elif choice == 3:
+				x_train, x_test, y_train, y_test = preprocessing(df, events=False)
+				model = LSTMModel(x_train.shape[1], y_train.shape[1])
 
-                intersection_idx = search(intersections, 'name', sel_intersection)
-                model_file = "model/" + sel_intersection + ".hdf"
-                if os.path.exists(model_file):
-                    model.load_network(model_file)
-                else:
-                    print("Model does not exist, starting from scratch")
-                    model.init_network(hidden_size=50)
-            try:
-                e = int(input("Enter Epochs to train for (Default=50): "))
-                model.epochs = e
-            except ValueError:
-                print("Invalid number entered, using default value")
-            model.train(x_train, y_train, model_file)
+				intersection_idx = search(intersections, 'name', sel_intersection)
+				model_file = "model/" + sel_intersection + ".hdf"
+				if os.path.exists(model_file):
+					os.remove(model_file)
+				model.init_network(hidden_size=50)
+			elif choice == 4:
+				x_train, x_test, y_train, y_test = preprocessing(df, events=False)
+				model = LSTMModel(x_train.shape[1], y_train.shape[1])
 
-            intersections[intersection_idx]['model'] = model_file
-            save_intersections(intersections, "intersections.data")
+				intersection_idx = search(intersections, 'name', sel_intersection)
+				model_file = "model/" + sel_intersection + ".hdf"
+				if os.path.exists(model_file):
+					model.load_network(model_file)
+				else:
+					print("Model does not exist, starting from scratch")
+					model.init_network(hidden_size=50)
+			try:
+				e = int(input("Enter Epochs to train for (Default=50): "))
+				model.epochs = e
+			except ValueError:
+				print("Invalid number entered, using default value")
+			model.train(x_train, y_train, './'+ model_file)
 
-        elif choice == 4:
-            # Test Intersections
-            model_file = "model/" + sel_intersection + ".hdf"
-            if os.path.exists(model_file):
-                model.load_network(model_file)
-                test_output = model.predict(x_test, y_test)
-                N, S, E, W = confusion_matrix(test_output, y_test, True)
-            else:
-                print("Please train intersection first")
+			intersections[intersection_idx]['model'] = model_file
+			save_intersections(intersections, "intersections.data")
 
-        elif choice == 5:
-            # Route Check
-            print("  Not implemented yet, please check back later")
+		elif choice == 4:
+			# Test Intersections
+			model_file = "model/" + sel_intersection + ".hdf"
+			model = LSTMModel(x_train.shape[1], y_train.shape[1])
+			if os.path.exists(model_file):
+				model.load_network(model_file)
+				test_output = model.get_accuracy(x_test, y_test)
+				N, S, E, W = confusion_matrix(test_output, y_test, True)
+			else:
+				print("Please train intersection first")
 
+		elif choice == 5:
+			model = LSTMModel(x_train.shape[1], y_train.shape[1], intersection_list)
+			# Route Check
+			x_data = []
+			day_week = [1,2,3,4,5,6,7]
+			season=[1,2,3,4]
+			time_str = ''
+			flag = 0
+			x_data = []
+			peak = 0
 
+			while flag == 0:
+				num_inter = input("Please enter intersection number: ")
+				if num_inter in intersection_list:
+					flag = 1
+					num_inter = int(num_inter)
+					#x_data.append(int(num_inter))
+				else:
+					print("Intersection not found!")
+					
+			flag = 0
+			while flag == 0:
+				week = [0,0,0,0,0,0,0]
+				num_day = input("Please enter the day of the week:\nOptions:\n1:Sunday\n2:Monday\n3:Tuesday\n4:Wednesday\n5:Thursday\n6:Friay\n7:Saturday\n")
+				num_day = int(num_day)
+				if num_day in day_week:
+					flag = 1
+					week[num_day-1] = 1
+					x_data = x_data + week
+				else:
+					print("Day of the week not found!")
+			
+			flag = 0
+			while flag == 0:
+				time_day = input("Please enter the time of the day (ex. 17:30): ")
+				temp = time_day.split(':')
+				if len(temp) == 2:
+					hour = int(temp[0]) * 60
+					if hour > 9 and hour < 17:
+						peak = 1
+					time = hour + int(temp[1])
+					time_d = float(time) / float(1440)
+					if time_d > 1.0:
+						print("Please enter time in the proper format!")
+					else:
+						x_data.append(time_d)
+						flag = 1
+				else:
+					print("Please enter time in the proper format!")
+			
+			flag = 0
+			while flag == 0:
+				seasons = [0,0,0,0]
+				season_input = input("Please enter the season:\n1:Summer\n2:Fall\n3:Winter\n4:Spring\n")
+				season_input = int(season_input)
+				
+				if season_input in season:
+					flag = 1
+					seasons[season_input-1] = 1
+					x_data = x_data + seasons
+				else:
+					print("Season not found!")
+			
+			# add [0,0] for events
+			x_data.append(peak)
+			x_data = x_data + [0,0]
+			x_test = np.array([x_data])
+			x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+			res = model.predict(x_test, num_inter)
+			print("Prediction: " + str(res))
+			
 main()
